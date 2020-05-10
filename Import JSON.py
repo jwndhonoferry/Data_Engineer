d8a36b805d069632e322e06beb2d1a59ad3c4236
Ferry Jiwandhono <ferryjwndhono@gmail.com>
2020-05-10 22:13:37 +0700

    tambah JSON API

--- Import JSON.py
+++ Import JSON.py
@@ -4,7 +4,7 @@ Created on Sun May 10 12:12:35 2020
 
 @author: jiwandhono
 """
-
+import requests
 from sqlalchemy import create_engine
 import pandas as pd
 from configparser import ConfigParser
@@ -26,10 +26,48 @@ try:
     df = pd.read_json("example_2.json", orient = "index")
     
     # Plot total population in shelters over time
-    # df["quiz"] = pd.to_datetime(df["quiz"])
-    # df.plot(x=columns, 
+    data_sport = pd.DataFrame(df["sport"])
+    data_maths = pd.DataFrame(df["maths"])
+    #df.plot(x=columns, 
     #         y=row)
     # plt.show()
     
 except ValueError:
     print("pandas could not parse the JSON.")
+    
+    
+    
+# =============================================================================
+# Introduction to APIs
+# =============================================================================
+api_url = "https://api.yelp.com/v3/businesses/search"
+api_key = "a"
+params = {"term" : "cafe", "location" : "NYC"}
+headers = {"Authorization": "Bearer {}".format(api_key)}
+# Get data about NYC cafes from the Yelp API
+response = requests.get(api_url, 
+                headers=headers, 
+                params=params)
+
+# Extract JSON data from the response
+data = response.json()
+
+# Load data to a data frame
+cafes = pd.DataFrame(data["businesses"])
+
+# View the data's dtypes
+print(cafes.dtypes)
+print(cafes.head())
+
+
+
+
+
+
+
+
+
+
+
+
+
